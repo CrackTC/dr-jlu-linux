a973b48a27acb7c9bfc5c26beeb0d89f06a8654e
CrackTC <chenrke369@gmail.com>
2022-12-07 19:34:57 +0800

    add README.md & remove unused files

---
+++ Login/main.cpp
@@ -0,0 +1,37 @@
+#include "./dogcom/login.h"
+
+// 登录信息
+struct tagLogInfo logInfo{"***REMOVED***", "***REMOVED***"};
+const char* strBuf = "***REMOVED***";
+
+int status = OFFLINE;
+
+// 收到的报文中的ip地址
+unsigned char receivedIp[4];
+
+
+char getCharFromInt(int x) {
+	if (x <= 9 && x >= 0) return (char)(x + '0');
+	return (char)(x - 10 + 'a');
+}
+int getIntFromChar(char x) {
+	if (x <= '9' && x >= '0') return x - '0';
+	return x - 'a' + 10;
+}
+
+
+void load() {
+    std::printf("load account: %s\n", logInfo.account);
+	std::printf("load pass: %s\n", logInfo.pass);
+	std::printf("load mac: %s\n", strBuf);
+	for (int i = 0; i < 6; i++) {
+		logInfo.mac[i] = (unsigned char)((getIntFromChar(strBuf[2 * i]) << 4)
+						| getIntFromChar(strBuf[2 * i + 1]));
+	}
+}
+
+int main()
+{
+    load();
+    login(logInfo.account, logInfo.pass, logInfo.mac);
+}
--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# dr-jlu-win32
-吉大windows带用户界面版Dr校园网登录客户端
+# dr-jlu-linux
+吉大Linux端Dr校园网登录客户端
 
-下载地址 https://github.com/code4lala/dr-jlu-win32/releases
-
-需要开机自启的同学请复制一份快捷方式到
-
-C:\Users\\<用户名\>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
-
-目录即可
-
-特别感谢 https://github.com/mchome/dogcom 网络通信部分代码复制粘贴过来的
-
-感谢 https://github.com/drcoms/jlu-drcom-client/tree/master/jlu-drcom-java 
+移植自 https://github.com/code4lala/dr-jlu-win32
